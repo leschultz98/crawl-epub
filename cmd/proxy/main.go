e3c476382c2c983c2c13716c2b95f471aec81283
phuc.le <leschultz98@outlook.com.vn>
2023-12-03 22:09:43 +0700

    refactor: proxy

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -1,100 +1,69 @@
 package main
 
 import (
-	"flag"
 	"io"
 	"log"
-	"net"
 	"net/http"
-	"strings"
+	"os"
 )
 
-// Hop-by-hop headers. These are removed when sent to the backend.
-// http://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html
-var hopHeaders = []string{
-	"Connection",
-	"Keep-Alive",
-	"Proxy-Authenticate",
-	"Proxy-Authorization",
-	"Te", // canonicalized version of "TE"
-	"Trailers",
-	"Transfer-Encoding",
-	"Upgrade",
-}
+const host = "https://truyenchu.vn"
 
-func copyHeader(dst, src http.Header) {
-	for k, vv := range src {
-		for _, v := range vv {
-			dst.Add(k, v)
-		}
-	}
-}
+var customTransport = http.DefaultTransport
 
-func delHopHeaders(header http.Header) {
-	for _, h := range hopHeaders {
-		header.Del(h)
-	}
-}
-
-func appendHostToXForwardHeader(header http.Header, host string) {
-	// If we aren't the first proxy retain prior
-	// X-Forwarded-For information as a comma+space
-	// separated list and fold multiple headers into one.
-	if prior, ok := header["X-Forwarded-For"]; ok {
-		host = strings.Join(prior, ", ") + ", " + host
-	}
-	header.Set("X-Forwarded-For", host)
-}
-
-type proxy struct {
-}
-
-func (p *proxy) ServeHTTP(wr http.ResponseWriter, req *http.Request) {
-	log.Println(req.RemoteAddr, " ", req.Method, " ", req.URL)
-
-	if req.URL.Scheme != "http" && req.URL.Scheme != "https" {
-		msg := "unsupported protocal scheme " + req.URL.Scheme
-		http.Error(wr, msg, http.StatusBadRequest)
-		log.Println(msg)
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	// Create a new HTTP request with the same method, URL, and body as the original request
+	proxyReq, err := http.NewRequest(r.Method, host+r.URL.String(), r.Body)
+	if err != nil {
+		http.Error(w, "Error creating proxy request", http.StatusInternalServerError)
 		return
 	}
 
-	client := &http.Client{}
-
-	//http: Request.RequestURI can't be set in client requests.
-	//http://golang.org/src/pkg/net/http/client.go
-	req.RequestURI = ""
-
-	delHopHeaders(req.Header)
-
-	if clientIP, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
-		appendHostToXForwardHeader(req.Header, clientIP)
+	// Copy the headers from the original request to the proxy request
+	for name, values := range r.Header {
+		for _, value := range values {
+			proxyReq.Header.Add(name, value)
+		}
 	}
 
-	resp, err := client.Do(req)
+	// Send the proxy request using the custom transport
+	resp, err := customTransport.RoundTrip(proxyReq)
 	if err != nil {
-		http.Error(wr, "Server Error", http.StatusInternalServerError)
-		log.Fatal("ServeHTTP:", err)
+		http.Error(w, "Error sending proxy request", http.StatusInternalServerError)
+		return
 	}
 	defer resp.Body.Close()
 
-	log.Println(req.RemoteAddr, " ", resp.Status)
+	// Copy the headers from the proxy response to the original response
+	for name, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(name, value)
+		}
+	}
 
-	delHopHeaders(resp.Header)
+	// Set the status code of the original response to the status code of the proxy response
+	w.WriteHeader(resp.StatusCode)
 
-	copyHeader(wr.Header(), resp.Header)
-	wr.WriteHeader(resp.StatusCode)
-	io.Copy(wr, resp.Body)
+	// Copy the body of the proxy response to the original response
+	io.Copy(w, resp.Body)
 }
 
 func main() {
-	var addr = flag.String("addr", "127.0.0.1:8080", "The addr of the application.")
-	flag.Parse()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
 
-	handler := &proxy{}
+	// Create a new HTTP server with the handleRequest function as the handler
+	server := http.Server{
+		Addr:    ":" + port,
+		Handler: http.HandlerFunc(handleRequest),
+	}
 
-	log.Println("Starting proxy server on", *addr)
-	if err := http.ListenAndServe(*addr, handler); err != nil {
-		log.Fatal("ListenAndServe:", err)
+	// Start the server and log any errors
+	log.Printf("Started at: http://localhost:%s\n", port)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Error starting proxy server: ", err)
 	}
 }
